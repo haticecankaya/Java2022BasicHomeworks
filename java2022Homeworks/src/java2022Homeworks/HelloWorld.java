b6f1129b25ed37222699d41eaa3cae0903570a68
hatic <hatic@host.docker.internal>
2022-09-22 15:00:14 +0300

    Java Lesson 5 in Engin Demiroğ Youtube Channel

---
+++ java2022Homeworks/src/java2022Homeworks/HelloWorld.java
@@ -0,0 +1,20 @@
+package java2022Homeworks;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//Comment Line 
+		//IntelliSense
+		System.out.println("Hello World!");
+        // Code blocks end with ; in Java
+		System.
+		out.
+		         println
+		         ("Hello World with not clean code!!"
+		        		 );
+		
+		
+		
+	}
+
+}
