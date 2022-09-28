39c8079615c04cc4305a3c8ed0031731bbfc8428
hatice <hatic@Hatice.mshome.net>
2022-09-28 18:22:50 +0300

    Java Lesson 13 in Engin Demiroğ Youtube Channel

---
+++ java2022Homeworks/src/java2022Homeworks/DoWhileLoop.java
@@ -0,0 +1,24 @@
+package java2022Homeworks;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int i = 1;
+		do {
+			System.out.println(i);
+			i += 1;
+		} while (i < 10);
+
+		System.out.println("The do-while loop finished");
+
+		int j = 50;
+		do {
+			System.out.println(j);
+			j += 1;
+		} while (j < 10);
+
+		System.out.println("The do-while loop finished");
+
+	}
+
+}
