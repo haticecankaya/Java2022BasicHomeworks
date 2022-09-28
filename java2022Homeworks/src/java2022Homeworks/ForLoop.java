3033ee732fcec47bbc751992fb3a831341e32799
hatic <hatic@Hatice.mshome.net>
2022-09-28 18:07:45 +0300

    Java Lesson 11 in Engin Demiroğ Youtube Channel

---
+++ java2022Homeworks/src/java2022Homeworks/ForLoop.java
@@ -0,0 +1,36 @@
+package java2022Homeworks;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			System.out.println(i);
+		}
+
+		System.out.println("The for loop finished");
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+
+		System.out.println("The for loop finished");
+
+		for (int i = 1; i <= 10; i += 2) {
+			System.out.println(i);
+		}
+
+		System.out.println("The for loop finished");
+
+		for (int i = 2; i <= 10; i = i + 2) {
+			System.out.println(i);
+		}
+
+		System.out.println("The for loop finished");
+
+		for (int i = 3; i <= 10; i += 3) {
+			System.out.println(i);
+		}
+
+	}
+
+}
