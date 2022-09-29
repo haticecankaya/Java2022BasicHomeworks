7a93bd5b8c30896497498e6c0318dfb2c7533a40
hatice <hatic@Hatice.mshome.net>
2022-09-29 14:08:23 +0300

    Java Lesson 18 in Engin Demiroğ Youtube Channel

---
+++ java2022Homeworks/src/java2022Homeworks/StringsDemo2.java
@@ -0,0 +1,37 @@
+package java2022Homeworks;
+
+public class StringsDemo2 {
+
+	public static void main(String[] args) {
+		/*
+		 * Change ; multiple; lines
+		 * 
+		 * ; to ; comment ; lines;
+		 */
+
+		String message = "My name is Hatice.";
+
+		message = message.replace(' ', '-');
+
+		System.out.println(message);
+		System.out.println(message.substring(5));
+		System.out.println(message.substring(5, 9));
+
+		for (String word : message.split("-")) {
+			System.out.println(word);
+
+		}
+
+		System.out.println(message.toLowerCase());
+		System.out.println(message.toUpperCase());
+		System.out.println(message);
+
+		message = "    " + message + "     ";
+		System.out.println(message);
+
+		message = message.trim();
+		System.out.println(message + "trim control");
+		System.out.println(message);
+	}
+
+}
